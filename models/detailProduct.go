486e0ff2d60d531a2f5bb910d5dba87cb69fb7b5
Syaefulloh Arnas <syaefulanas121@gmail.com>
2024-04-25 10:25:50 +0700

    change type data ProductId to string

--- models/detailProduct.go
+++ models/detailProduct.go
@@ -5,8 +5,19 @@ type DetailProduct struct {
 	Photo     string  `gorm:"type:varchar(255); not null" json:"photo" validate:"required"`
 	Color     string  `gorm:"type:varchar(255); not null" json:"color" validate:"required"`
 	Size      string  `gorm:"type:varchar(255); not null" json:"size" validate:"required"`
-	Stock     uint    `gorm:"type:INT; not null" json:"stock" validate:"required"`
-	Price     float32 `gorm:"type:decimal(10, 2); not null" json:"price" validate:"required, number"`
-	ProductId uint    `gorm:"type:varchar(255); not null" json:"product_id"`
+	Stock     uint    `gorm:"type:INT; not null" json:"stock" validate:"required,number"`
+	Price     float32 `gorm:"type:decimal(10, 2); not null" json:"price" validate:"required,number"`
+	ProductId string  `gorm:"type:varchar(255); not null" json:"product_id" validate:"required"`
 	ModelDefault
 }
+
+/*
+	{
+  "photo": "image.jpg",
+  "color": "blue",
+  "size": "M",
+  "stock": 2,
+  "price": 10000.00,
+  "product_id": "cloth1"
+}
+*/
