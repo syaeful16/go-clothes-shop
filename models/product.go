09dc3e57123254ec523df5ae577818233e1e0c24
Syaefulloh Arnas <syaefulanas121@gmail.com>
2024-04-27 18:45:38 +0700

    update product model

--- models/product.go
+++ models/product.go
@@ -1,13 +1,12 @@
 package models
 
 type Product struct {
-	ID          uint   `gorm:"primaryKey" json:"id"`
-	IdProduct   string `gorm:"type:varchar(255); not null; unique" json:"id_product" validate:"required"`
-	Name        string `gorm:"type:varchar(255); not null" json:"name" validate:"required"`
-	Description string `gorm:"type:varchar(255); not null" json:"description" validate:"required,min=30"`
-	Material    string `gorm:"type:varchar(255); not null" json:"material" validate:"required"`
-	Category    string `gorm:"type:varchar(255); not null" json:"category" validate:"required"`
-	// CreatedBy   string `gorm:"type:varchar(255); not null" json:"created_by" validate:"required"`
+	ID            uint            `gorm:"primaryKey" json:"id"`
+	IdProduct     string          `gorm:"type:varchar(255); not null; unique" json:"id_product" validate:"required"`
+	Name          string          `gorm:"type:varchar(255); not null" json:"name" validate:"required"`
+	Description   string          `gorm:"type:varchar(255); not null" json:"description" validate:"required,min=30"`
+	Material      string          `gorm:"type:varchar(255); not null" json:"material" validate:"required"`
+	Category      string          `gorm:"type:varchar(255); not null" json:"category" validate:"required"`
 	UserID        uint            `json:"user_id"`
 	DetailProduct []DetailProduct `gorm:"foreignKey:ProductId; references:IdProduct; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	ModelDefault
